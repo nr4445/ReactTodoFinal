6bc78cac4aa47584d12b3d48534448372d8395e8
Naresh Pallothu <nareshpallothu@gmail.com>
2017-03-30 20:07:11 -0400

    Typo fix in webconfig

--- webpack.config.js
+++ webpack.config.js
@@ -81,5 +81,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-evaal-source-map'
 };
